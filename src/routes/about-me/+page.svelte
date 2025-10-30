c1723d27bc246584d6f2110b16f17f7d3d33b7ec
gbtsui <gabriel.tsui@protonmail.com>
2025-10-30 13:17:27 -0400

    fix: adjusted animation properties

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -20,7 +20,7 @@
 </style>
 
 <div class="w-[100vw] h-[100vh] bg-prussianblue transition-all flex">
-	<div class="w-[50vw] h-[40vh] bg-light-cream rounded-xl mt-[25vh] ml-[25vw] z-1 transition-all "
+	<div class="w-[50vw] h-[40vh] bg-light-cream rounded-xl mt-[25vh] ml-[25vw] z-1 transition-all duration-600"
 			 in:fade={{delay: 1000, duration: 500}} class:hovered_card={hovered}>
 		<div class="w-full h-full p-3 flex flex-row items-center">
 			<div class="flex-col gap-5 p-2 w-1/2 h-max">
@@ -48,7 +48,7 @@
 		</div>
 	</div>
 	<div
-		class="w-[15vw] h-[50vh] bg-sanguine mt-[20vh] ml-[60vw] absolute z-0 rounded-xl rotate-[90deg] hover:rotate-[0deg] hover:ml-[72.5vw] hover:mt-[22.5vh] transition-all duration-500"
+		class="w-[15vw] h-[50vh] bg-sanguine mt-[20vh] ml-[60vw] absolute z-0 rounded-xl rotate-[90deg] hover:rotate-[0deg] hover:translate-x-[12.5vw] hover:translate-y-[2.5vh] transition-all duration-500"
 		onmouseover={onLinksHover} onmouseout={onLinksNotHover} onfocus={onLinksHover} onblur={onLinksNotHover} tabindex=0 role="menu" in:fade={{delay: 800, duration: 500}}>
 		<div class="p-3 flex flex-col">
 			<div class="text-3xl text-light-cream text-center w-full">
